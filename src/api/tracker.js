95f0295d16bfb55dab9e6a91a59db409c4ad9bd1
ANKITA SINHA <ankitasinha41@gmail.com>
2020-06-22 16:42:10 +0530

    Second Commit

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,4 +1,4 @@
 import axios from 'axios'
 export default axios.create({
-    baseURL:'https://18040a4cbe25.ngrok.io'
+    baseURL:'http://aec957f7e071.ngrok.io'
 })
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,19 +1,22 @@
 import "../_mockLocation";
-import React, {useContext } from "react";
+import React, {useContext,useCallback } from "react";
 import { StyleSheet } from "react-native";
 import { Text } from "react-native-elements";
 import { SafeAreaView,withNavigationFocus } from "react-navigation";
 import useLocation from '../hooks/useLocation'
 import Map from "../components/Map";
 import { Context as LocationContext } from "../context/LocationContext";
+import TrackForm from '../components/TrackForm'
 
 const TrackCreateScreen = ({isFocused}) => {
-  const { addLocation } = useContext(LocationContext);
-  const [err] = useLocation((isFocused,location) => {
-    addLocation(location)
-  })
+  const { state : {recording},addLocation } = useContext(LocationContext);
+  const callback = useCallback((location)=> {
+    addLocation(location,recording)
+
+  },[recording])
+  const [err] = useLocation(isFocused || recording,callback) 
   
-  console.log(isFocused)
+  //console.log(isFocused)
   
   return (
     <SafeAreaView forceInset={{ top: "always" }}>
@@ -21,6 +24,7 @@ const TrackCreateScreen = ({isFocused}) => {
       <Map />
       {/*<NavigationEvents onWillBlur = {() => {console.log('Leaving...')}}/>*/}
       {err ? <Text>Please enable location services</Text> : null}
+      <TrackForm/>
     </SafeAreaView>
   );
 };
